6e5a938643e99ae4dbe717407fa607d0ceec5adf
easywaldo <easywaldoji@gmail.com>
2022-06-26 22:39:52 +0900

    generic replace base class

---
+++ models/generic_inherit.py
@@ -0,0 +1,13 @@
+from typing import TypeVar, Generic
+from pydantic.generics import GenericModel
+
+TypeX = TypeVar('TypeX')
+
+class BaseClass(GenericModel, Generic[TypeX]):
+    X: TypeX
+    
+class ChildClass(BaseClass[TypeX], Generic[TypeX]) :
+    pass
+
+# Replace TypeX by int
+print(ChildClass[int](X=1))
