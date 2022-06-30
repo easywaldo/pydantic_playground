e6aee0d036b86ae8ea70f11d78283e93b28f1485
easywaldo <easywaldoji@gmail.com>
2022-07-01 01:10:16 +0900

    new line added

--- models/custom_root_types.py
+++ models/custom_root_types.py
@@ -7,12 +7,12 @@ class Pets(BaseModel):
     __root__: List[str]
     
 print(Pets(__root__=['dog', 'cat']))
-
+print()
 print(Pets(__root__=['dog', 'cat']).json())
-
+print()
 print(Pets.parse_obj(['dog', 'cat']))
-
+print()
 print(Pets.schema())
-
+print()
 pets_schema = schema([Pets])
 print(json.dumps(pets_schema, indent=4))
