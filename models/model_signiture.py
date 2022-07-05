c49e08442265a3a0058e79d52808b5eddf91f272
easywaldo <easywaldoji@gmail.com>
2022-07-05 23:53:30 +0900

    model signiture

--- models/model_signiture.py
+++ models/model_signiture.py
@@ -8,3 +8,16 @@ class FooModel(BaseModel):
     apple: int = Field(..., alias='pear')
     
 print(inspect.signature(FooModel))
+
+
+
+
+class MyModel(BaseModel):
+    id: int
+    info: str = 'Foo'
+    
+    def __init__(self, id: int = 1, *, bar: str, **data) -> None:
+        """My custom int"""
+        super().__init__(id=id, bar=bar, **data)
+
+print(inspect.signature(MyModel))
