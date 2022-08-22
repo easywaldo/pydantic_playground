493594768c58ad3e1c25d3a2cc8484049dca561f
easywaldo <easywaldoji@gmail.com>
2022-08-23 01:15:24 +0900

    subclass constructor

--- field_types/type_field.py
+++ field_types/type_field.py
@@ -4,21 +4,27 @@ from pydantic import BaseModel
 from pydantic import ValidationError
 
 class Foo:
-    pass
+    def __init__(self, name: str):
+        self.name = name
 
 class Bar(Foo):
-    pass
+    def __init__(self, name: str, description: str):
+        super.name = name
+        self.description = description
 
 class Other:
-    pass
+    def _init(self, name: str, description: str, etc: str):
+        super.name = name
+        super.description = description
+        self.etc = etc
 
 class SimpleModel(BaseModel):
     just_subclasses: Type[Foo]
     
-SimpleModel(just_subclasses=Foo)
-SimpleModel(just_subclasses=Bar)
+SimpleModel(just_subclasses=Foo(name="foo"))
+SimpleModel(just_subclasses=Bar(description="desc"))
 
 try:
-    SimpleModel(just_subclasses=Other)
+    SimpleModel(just_subclasses=Other(etc="etc"))
 except ValidationError as e:
     print(e)
