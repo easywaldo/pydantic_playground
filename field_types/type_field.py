fb7d56a4902edc5c9016dad0d6ea49e2804c6059
easywaldo <easywaldoji@gmail.com>
2022-08-23 23:13:32 +0900

    잘못된 subclass constructor 수정

--- field_types/type_field.py
+++ field_types/type_field.py
@@ -9,22 +9,22 @@ class Foo:
 
 class Bar(Foo):
     def __init__(self, name: str, description: str):
-        super.name = name
+        super().__init__(name)
         self.description = description
 
 class Other:
-    def _init(self, name: str, description: str, etc: str):
-        super.name = name
-        super.description = description
+    def __init__(self, name: str, description: str, etc: str):
+        self.name = name
+        self.description = description
         self.etc = etc
 
 class SimpleModel(BaseModel):
     just_subclasses: Type[Foo]
     
-SimpleModel(just_subclasses=Foo(name="foo"))
-SimpleModel(just_subclasses=Bar(description="desc"))
+SimpleModel(just_subclasses=type(Foo(name="foo")))
+SimpleModel(just_subclasses=type(Bar(name="waldo", description="desc")))
 
 try:
-    SimpleModel(just_subclasses=Other(etc="etc"))
+    SimpleModel(just_subclasses=type(Other(name="wrong", description="none", etc="etc")))
 except ValidationError as e:
     print(e)
